9657367d2875731ff332acd3f206e3fa846f4706
GammaFivez <72508380+GammaFivez@users.noreply.github.com>
2021-05-26 22:46:19 +0100

    Fixed issue

--- work uni/MobileSystemsDevelopmentMultiTool-main/MobileSystemsDevelopmentMultiTool-main/MultiTool/app/src/main/java/com/example/multitool/settingStorage.java
+++
@@ -1,9 +0,0 @@
-package com.example.multitool;
-public class settingStorage {
-public static class dataStorage{
-    public static boolean isAlarm1Enabled = false;
-    public static boolean isAlarm2Enabled = false;
-    public static boolean isDarkModeEnabled = false;
-}
-
-}
